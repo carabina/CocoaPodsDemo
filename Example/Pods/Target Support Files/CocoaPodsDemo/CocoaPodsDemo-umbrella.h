2c4bc61ebdd954a55dc78f0ffbb1a2aa432c6a84
SwiftLOL <SwiftLOL@163.com>
2016-08-11 00:55:02 +0800

    setup respo

---
+++ Example/Pods/Target Support Files/CocoaPodsDemo/CocoaPodsDemo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double CocoaPodsDemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char CocoaPodsDemoVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-CocoaPodsDemo_Example/Pods-CocoaPodsDemo_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CocoaPodsDemo_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CocoaPodsDemo_ExampleVersionString[];
+
